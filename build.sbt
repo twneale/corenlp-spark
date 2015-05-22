260d84338db51a5e3dc83186dbe6406961b29a85
twneale <twneale@gmail.com>
2015-05-22 15:40:00 -0400

    Moved stuff

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "corenlp-spark"
+
+version := "0.0"
+
+scalaSource in Compile := baseDirectory.value / "src"
+
+resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.3.1",
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.4",
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.4" classifier "models",
+  "edu.stanford.nlp" % "stanford-parser" % "3.4",
+  "com.typesafe.play" %% "play-json" % "2.2.1"
+  )
---
+++ src/main/scala/SparkCoreNLP.scala
@@ -0,0 +1,44 @@
+import java.util.Properties
+import java.io.{ByteArrayOutputStream,StringWriter,PrintWriter}
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+
+import play.api.libs.json._
+
+
+object SparkJob {
+  def main(args: Array[String]) {
+    var props = new Properties
+    val annotators = args(0)
+    val input = args(1)
+    val output = args(2)
+    val numpartitions = args(3).toInt
+
+    val conf = new SparkConf()
+    conf.setAppName(s"CoreNLP($annotators) - $input - $output")
+    val sc = new SparkContext(new SparkConf())
+
+    props.setProperty("annotators", annotators)
+    object CoreNLP {
+      @transient lazy val pipeline = new StanfordCoreNLP(props)
+      def getXML(sentence: String): String = {
+          val document = new Annotation(sentence)
+          pipeline.annotate(document)
+          var output_stream = new ByteArrayOutputStream
+          pipeline.xmlPrint(document, output_stream)
+          return output_stream.toString("utf8");
+        }
+      }
+
+    var data = sc.textFile(input)
+    data = data.repartition(numpartitions)
+    val xml = data.map(s => CoreNLP.getXML(s))
+    xml.saveAsTextFile(output)
+    }
+}
+
